dcff8321fd0e03b46ffd7b02bc35592f0550131f
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-06-18 01:35:33 -0700

    Create AutoAnnounceCommand.php

---
+++ src/wock/announcer/Commands/AutoAnnounceCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace wock\announcer\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\utils\TextFormat;
+use wock\announcer\AutoAnnouncer;
+
+class AutoAnnounceCommand extends Command implements PluginOwned {
+
+    /** @var AutoAnnouncer */
+    private AutoAnnouncer $plugin;
+
+    public function __construct(AutoAnnouncer $plugin) {
+        parent::__construct("autoannouncer", "Reload the AutoAnnouncer configuration", "/autoannouncer", ["aa"]);
+        $this->setPermission("autoannounce.reload");
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
+    {
+        if (!$sender->hasPermission("autoannounce.reload")) {
+            $sender->sendMessage(TextFormat::RED . "You do not have sufficient permission to use this command!");
+            return false;
+        }
+
+        $this->plugin->reloadConfig();
+        $sender->sendMessage(TextFormat::GREEN . "Successfully reloaded configuration.");
+        return true;
+    }
+
+    public function getOwningPlugin(): AutoAnnouncer
+    {
+        return $this->plugin;
+    }
+}
+
